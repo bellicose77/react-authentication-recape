6709709d3b747f77b4564fd53880db78181e5a7f
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-03-28 00:32:06 +0600

    firebase install done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC7W2wgFA203nzQMPb9hly2rQ-F_ZDSaSk",
+    authDomain: "react-authentication-reacape.firebaseapp.com",
+    projectId: "react-authentication-reacape",
+    storageBucket: "react-authentication-reacape.appspot.com",
+    messagingSenderId: "559970104576",
+    appId: "1:559970104576:web:3a06285c1bb88ae1546fe5"
+  };
+  export default firebaseConfig;
