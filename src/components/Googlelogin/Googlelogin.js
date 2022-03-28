6e6a0e565311cebd2645d7b1ed23c08d6f3dbed5
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-03-28 21:07:57 +0600

    state hook done

--- src/components/Googlelogin/Googlelogin.js
+++ src/components/Googlelogin/Googlelogin.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GoogleAuthProvider,getAuth,signInWithPopup, initializeAuth } from "firebase/auth";
 const provider = new GoogleAuthProvider();
 const Googlelogin = () => {
+    const [user,setUser]=useState('')
     const handleGoogleSignIN = () =>{
         const auth = getAuth();
         signInWithPopup(auth,provider)
         .then((result) => {
           const loginuser = result.user;
-           console.log(loginuser)
+           setUser(loginuser)
         })
         console.log("Button works")
       }
     return (
         <div>
           <button onClick={handleGoogleSignIN}>Google SignIN</button>  
+
+          <p>User Name:{user.displayName}</p>
+          <br/>
+          <p>User Email: {user.email}</p>
         </div>
     );
 };
