7a047232b10912deb8beca979878a3f729d09f7d
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-03-28 20:28:54 +0600

    button handle done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { GoogleAuthProvider } from "firebase/auth";
 
 function App() {
+  const handleGoogleSignIN = () =>{
+    console.log("Button works")
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <button onClick={handleGoogleSignIN}>Google SignIN</button>
     </div>
   );
 }
